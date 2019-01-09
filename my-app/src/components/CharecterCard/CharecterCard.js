5bfc4a3ad7cccb10305a1920a9edb7204dc44563
Bradley <brad.driver33@gmail.com>
2019-01-09 16:02:34 -0500

    Finished coding the Charecter Card directory and the files in them...mostly

---
+++ my-app/src/components/CharecterCard/CharecterCard.js
@@ -0,0 +1,19 @@
+import "./CharecterCard.css";
+
+const CharecterCard = props => (
+    <div className="card">
+    <div className="img-container">
+    <img alt={props.name} src={props.image} />
+    </div>
+    <div className="content">
+    <ul>
+        <li>
+            <strong>Name:</strong> {props.name}
+        </li>
+    </ul>
+    </div>
+    {/* Need and on click event for when the user clicks the charecter it increments the score.  Also need an event where if the user clicks the same charecter twice it resets the score to zero. */}
+    </div>
+);
+
+export default CharecterCard;
